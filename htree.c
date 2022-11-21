5cae8f97a976172e1cb5b0aa12eb8261e8e5bf70
Buchimi <stormzzy900@gmail.com>
2022-11-20 23:45:52 -0600

    last

--- htree.c
+++ htree.c
@@ -17,6 +17,8 @@
 
 // block size
 #define BSIZE 4096
+
+// Struct symbolizes the arguments we pass into the hashode function
 typedef struct args
 {
   int threadNo;
@@ -26,11 +28,20 @@ typedef struct args
 } args;
 
 void Usage(char *);
+// our hash function
 uint32_t jenkins_one_at_a_time_hash(const uint8_t *, uint64_t);
+
+// function to find the total hash
 void *findHash(void *args);
+
+// helper method to construct the args
 args *constructArgs(args *argument, int threadNo, int maxNoThreads, uint8_t *mapping, int length);
+// driver method to combine the hashes of different nodes
 uint32_t internalNodeHash(uint32_t hash, uint32_t *lefthash, uint32_t *rightHash);
+// to get the time
 double GetTime();
+
+// driver function to find each node's hash val
 uint32_t hashnode(args *thread, int blockperthread);
 
 int main(int argc, char **argv)
@@ -55,9 +66,10 @@ int main(int argc, char **argv)
   fstat(fd, &stats);
   // calculate nblocks
 
-  nblocks = stats.st_size / BSIZE;
+  nblocks = stats.st_size / BSIZE; // no of blocks
   int blocksperthread = nblocks / atoi(argv[2]);
 
+  // our file as a map
   uint8_t *map = (uint8_t *)mmap(NULL, stats.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
   printf("no. of blocks = %u \n", nblocks);
 
@@ -66,8 +78,11 @@ int main(int argc, char **argv)
   // calculate hash value of the input file
   int threadNo = 0;
   args *arguments = NULL;
+  // our args
   arguments = constructArgs(arguments, threadNo, atoi(argv[2]), map, blocksperthread);
   printf("Blocks per thread: %d\n", arguments->length);
+
+  // our driver function
   hash = *(uint32_t *)findHash((void *)arguments);
 
   double end = GetTime();
@@ -79,28 +94,31 @@ int main(int argc, char **argv)
 
 void *findHash(void *threadArgs)
 {
+  // initialize data
   pthread_t left = 0;
   pthread_t right = 0;
   args argu = *((args *)threadArgs);
-
-  // hash your data
 
   uint32_t *hash = malloc(sizeof(uint32_t));
   uint32_t *lefthash = NULL;
   uint32_t *righthash = NULL;
 
+  // find the member properties of our args
   int maxNoThreads = ((args *)threadArgs)->maxNoThreads;
   int threadNo = (((args *)threadArgs)->threadNo);
   int bpt = (((args *)threadArgs)->length);
 
+  // create args for a potential left and child node
   args *leftArgs = NULL;
   leftArgs = constructArgs(leftArgs, threadNo * 2 + 1, maxNoThreads, argu.mmapLink, bpt);
 
   args *rightArgs = NULL;
   rightArgs = constructArgs(rightArgs, threadNo * 2 + 2, maxNoThreads, argu.mmapLink, bpt);
 
+  // check to see if a left child's number is beyond what we need
   if (leftArgs->threadNo < maxNoThreads)
   {
+
     int success = pthread_create(&left, NULL, findHash, (void *)leftArgs);
     if (success == 1)
     {
@@ -110,7 +128,9 @@ void *findHash(void *threadArgs)
   }
   else
   {
+    // if we don't have a left child, we just hash and return
     *hash = hashnode(&argu, bpt);
+
     // child node
     if (argu.threadNo == 0)
     {
@@ -120,12 +140,16 @@ void *findHash(void *threadArgs)
     pthread_exit(hash);
   }
 
+  // right args
   if ((rightArgs->threadNo) < maxNoThreads)
   {
     pthread_create(&right, NULL, findHash, (void *)rightArgs);
   }
 
+  // hash our block
   *hash = hashnode(&argu, bpt);
+
+  // join threads if they exist
   if (left != 0)
   {
 
@@ -136,17 +160,20 @@ void *findHash(void *threadArgs)
   {
     pthread_join(right, (void **)&righthash);
   }
-  uint32_t temp = 0;
-  if (lefthash != NULL){
 
+  // temp is a temp holder of our hash
+  if (lefthash != NULL)
+  {
+    uint32_t temp = 0;
     temp = internalNodeHash(*hash, lefthash, righthash);
+    *hash = temp;
   }
-  *hash = temp;
-
+  //returns from functions if this thread is the root thread
   if (argu.threadNo == 0)
   {
     return hash;
   }
+  //else do a pthread exit
   pthread_exit(hash);
   // return val;
 }
@@ -177,6 +204,7 @@ void Usage(char *s)
 
 uint32_t hashnode(args *thread, int blockperthread)
 {
+  // calculate necessary parameters
   uint32_t offset = thread->threadNo;
   offset *= blockperthread * BSIZE;
   const uint8_t *inte = (const uint8_t *)(&(thread->mmapLink[offset]));
@@ -184,13 +212,16 @@ uint32_t hashnode(args *thread, int blockperthread)
   uint64_t len = BSIZE;
   len *= blockperthread;
 
+  // returns the jenkins hash
   return jenkins_one_at_a_time_hash(inte, len);
 }
 
 uint32_t internalNodeHash(uint32_t hash, uint32_t *lefthash, uint32_t *rightHash)
 {
+
   char *hash_string;
 
+  // combine strings then hash result
   if (rightHash == NULL)
   {
     hash_string = malloc(21);
@@ -206,27 +237,18 @@ uint32_t internalNodeHash(uint32_t hash, uint32_t *lefthash, uint32_t *rightHash
   return value;
 }
 
-int isLeaf(int nodeNo, int maxNoThreads)
-{
-  if (nodeNo * 2 + 1 <= maxNoThreads)
-  {
-    return 1;
-  }
-  return 0;
-}
-
 args *constructArgs(args *argument, int threadNo, int maxNoThreads, uint8_t *mapping, int length)
 {
+  // helps us constructs an args struct based on parameters
   argument = (args *)malloc(sizeof(args));
-
   argument->maxNoThreads = maxNoThreads;
-
   argument->threadNo = threadNo;
   argument->mmapLink = mapping;
   argument->length = length;
   return argument;
 }
 
+// calculates time
 double GetTime()
 {
   struct timeval t;
